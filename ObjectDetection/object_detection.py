881bff655715ed878f5da3602411590f2b05e29a
djm10 <djm9826@hotmail.com>
2023-09-20 14:56:50 -0500

    AB#22: Crear pipeline basico para deteccion de personas - AB#23: Crear la estructura base para deteccion de personas - Create BoundingBoxes class

--- ObjectDetection/object_detection.py
+++ ObjectDetection/object_detection.py
@@ -8,6 +8,48 @@ from torch import Tensor
 import numpy as np
 from ultralytics import YOLO
 from ultralytics.engine.results import Boxes
+
+class BoundingBoxes:
+
+    def __init__(self, boxes: Tensor, orig_shape: tuple[int], names: Dict[int,str]):
+        if boxes.ndim == 1:
+            boxes = boxes[None, :]
+
+        self.data = boxes
+        self.names = names
+        self.orig_shape = orig_shape
+
+    def cpu(self):
+        """Return a copy of the tensor on CPU memory."""
+        return self.__class__(self.data.cpu(), self.orig_shape, self.names)
+
+    def cuda(self):
+        """Return a copy of the tensor on GPU memory."""
+        return self.__class__(torch.as_tensor(self.data).cuda(), self.orig_shape, self.names)
+
+    def __len__(self):  # override len(results)
+        """Return the length of the data tensor."""
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """Return a BaseTensor with the specified index of the data tensor."""
+        return self.__class__(self.data[idx], self.orig_shape, self.names)
+
+    @property
+    def xyxy(self) -> Tensor:
+        """Return the boxes in xyxy format."""
+        return self.data[:, :4]
+
+    @property
+    def conf(self) -> Tensor:
+        """Return the confidence values of the boxes."""
+        return self.data[:, -2]
+
+    @property
+    def cls(self) -> Tensor:
+        """Return the class values of the boxes."""
+        return self.data[:, -1]
+
 class ObjectDetection(ABC):
     def __init__(self, model_path: str):
         
