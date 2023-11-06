d584fbce58bacd9af86a250a07223f10ccec88fd
Santiago <santiji00@gmail.com>
2023-11-06 18:50:59 -0500

    AB#27: RU4: Creacion de pipeline basico de extraccion de caracteristicas - AB#38: Detector de pies - Add position enums

--- src/feature/position_msg.py
+++ src/feature/position_msg.py
@@ -1,7 +1,13 @@
 import numpy as np
+from enum import Enum
 from dataclasses import dataclass
 
 @dataclass
 class PositionMsg:
     boxes: np.ndarray
-    type: str
+    type: "PositionType"
+
+class PositionType(str, Enum):
+    BAD = "Bad"
+    NORMAL = "Normal"
+    ESTIMATED = "Estimated"
